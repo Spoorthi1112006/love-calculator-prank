dd462b0622607ae0bcbb636b72f5aec753a940f6
Spoorthi <spoorthimg2006@gmail.com>
2026-02-16 00:37:51 +0530

    Fix root path serving

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+// Serve index.html on root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
+
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 // Database Connection
